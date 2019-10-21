d5a5e981e3e8005a99158a0d6f93613231acd5ef
Philip Durbin <philipdurbin@gmail.com>
2019-10-20 22:39:50 -0400

    reformat singup.html

--- util/reformat-all.sh
+++ util/reformat-all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 prettier --parser html --write testing.html
+prettier --parser html --write signup.html
 prettier --parser babel --write assets/js/testing.js
 prettier --parser babel --write assets/js/signup.js
