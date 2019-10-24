232f482e72093ee6da6ccb2afbe101605057c3be
Philip Durbin <philipdurbin@gmail.com>
2019-10-24 19:41:30 -0400

    reformat index.html and index.js

--- util/reformat-all.sh
+++ util/reformat-all.sh
@@ -2,6 +2,8 @@
 prettier --parser html --write testing.html
 prettier --parser html --write signup.html
 prettier --parser html --write admin.html
+prettier --parser html --write index.html
 prettier --parser babel --write assets/js/testing.js
 prettier --parser babel --write assets/js/signup.js
 prettier --parser babel --write assets/js/admin.js
+prettier --parser babel --write assets/js/index.js
