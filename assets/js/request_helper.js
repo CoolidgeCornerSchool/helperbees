b72cbbcfc3e86894cfcc76462a70fc8a83c08708
Steve Strassmann <steve@strassmann.com>
2019-11-08 00:59:52 -0500

    cleaner layout for request_helper

--- assets/js/request_helper.js
+++ assets/js/request_helper.js
@@ -5,14 +5,26 @@
 $(document).ready(init_shop);
 
 const offers_url = API_BASE_URL + '/offer';
+
+// All offers (includes many of the same offer_type)
 var offers = null;
+
+// enumerated distinct offer_types
 var offer_types = {};
+
+const test_offer = {
+    offer_type: 'testing',
+    offer_unit: 'item',
+    offer_per_hour: 1,
+    offer_description: 'Testing the payment and confirmation system'
+};
 
 function init_shop() {
     $.get(offers_url).done(on_load_offers);
     $('select#offer_type').change(on_change_offer_type);
 }
 
+// returns a <form/>
 function paypal_button(offer_id, offer_type){
     let url = "https://www.paypal.com/cgi-bin/webscr"
     let form = $('<form/>').attr({action: url, method: "post", target:"_top"});
@@ -42,20 +54,17 @@ function paypal_button(offer_id, offer_type){
     return form;
 }
 
-const test_offer = {
-    offer_type: 'testing',
-    offer_unit: 'item',
-    offer_per_hour: 1,
-    offer_description: 'Testing the payment and confirmation system'
-};
+
 
 function on_load_offers(data) {
     offers = data.result;
-    offers.push(test_offer);
+    // enumerate the distinct offer_types
     for (var i in offers) {
 	offer = offers[i];
 	offer_types[offer.offer_type] = true;
     }
+    offers.push(test_offer);    
+    // If url has hashtag (e.g. #babysitting) select it in menu
     let path = window.location.pathname;
     let hash = decodeURI(window.location.hash).replace('<slash>', '/');
     let chosen = null;
@@ -63,8 +72,11 @@ function on_load_offers(data) {
 	chosen = hash.substr(1);
     }
     let dropdown = $('select#offer_type');
+
+    // build dropdown menu from list of offer_types
     let ot_list = Object.keys(offer_types);
     ot_list.sort();
+    ot_list.push('testing');
     for (var i in ot_list) {
 	let offer_type = ot_list[i];
 	let option = $('<option/>')
@@ -78,57 +90,53 @@ function on_load_offers(data) {
     dropdown.change();
 }
 
+// User makes choice from pull-down menu
 function on_change_offer_type() {
     let offer_count = 0;
-  let type = $(this).val();
-  let offerdivs = $('table.offers').empty();
-  let header = $('<tr/>').append(
-    $('<th/>'),
-    $('<th/>').text('Description'),
-    $('<th/>').text('Helper Bee'),
-  );
-  offerdivs.append(header);
-  for (var i in offers) {
-    let offer = offers[i];
-    console.log(offer);
-    if (offer.offer_type == type) {
-      let team_cell = $('<td/>');
-	let kid = offer.user_id;
-	
-	//Kid information is redacted for now.
-	//kid_details = kids[kid];
-	//console.log(kid_details);
-	//let last_initial = kid_details.last_name.substring(0, 1);
-	//team_cell.append(kid_details.first_name + ' ' + last_initial);
-	
-      let type_cell = $('<td/>').text(
-        offer.offer_type +
-          ' (per ' +
-          ' ' +
-          offer.offer_unit +
-          ', number of ' +
-          offer.offer_unit +
-          's: ' +
-          offer.offer_per_hour +
-          '): ' +
-          offer.offer_description,
-      );
-	let buy_btn = $('<td/>').append(
-	    paypal_button(offer.offer_id, offer.offer_type)
-	);
-      let offer_row = $('<tr/>')
-        .append(buy_btn, type_cell, team_cell)
-        .addClass('align-items-center')
-        .attr('offer_id', offer.offer_id)
-        .attr('per_unit', offer.per_unit);
-	offerdivs.append(offer_row);
-	offer_count += 1;
+    let type = $(this).val();
+    let offer_table = $('table.offers').empty();
+    let header_row = $('<tr/>').append(
+	$('<th/>').css('width', '20%'),
+	$('<th/>').css('width', '60%').text('Description'),
+	$('<th/>').css('width', '20%').text('Helper Bee'),
+    );
+    offer_table.append($('<thead/>').append(header_row));
+
+    let tbody = $('<tbody/>');
+    offer_table.append(tbody);
+    for (var i in offers) {
+	let offer = offers[i];
+	if (offer.offer_type == type) {
+	    let row = render_offer_row(offer);
+	    tbody.append(row);
+	    offer_count += 1;
+	}
     }
-  }
     if (offer_count > 0){
 	$('table.offers').removeClass('d-none');
     } else {
 	$('table.offers').addClass('d-none');
     }	
- 
+}
+
+
+
+// returns a row div
+function render_offer_row(offer){
+    let type_txt = offer.offer_type;
+    if (type_txt == 'other' && offer.offer_type_other != '<empty>'){
+	type_txt += ': ' + offer.offer_type_other;
+    }
+    type_txt += ' (' + offer.offer_per_hour + ' per ' + offer.offer_unit + ') ';
+    let type = $('<div/>').text(type_txt);
+    let desc = $('<div/>').text(offer.offer_description);
+    let about = $('<td/>').append(type, desc);
+    let buy_btn = $('<td/>')
+	.append(paypal_button(offer.offer_id, offer.offer_type)
+	       );
+    let bee = $('<td/>');
+    return $('<tr/>')
+        .append(buy_btn, about, bee)
+        .addClass('align-items-center')
+        .attr('offer_id', offer.offer_id);
 }
